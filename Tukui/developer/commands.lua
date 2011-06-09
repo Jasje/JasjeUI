1485e5c63b9b3c9cdf7418392984a85bcee227a2
Jasje <bonkebesje@gmail.com>
2011-06-09 18:19:24 +0200

    removed test cause it doesnt work proper

--- Tukui/developer/commands.lua
+++ Tukui/developer/commands.lua
@@ -50,15 +50,6 @@ function SlashCmdList.LUAERROR(msg, editbox)
 end
 SLASH_LUAERROR1 = '/luaerror'
 
-SLASH_TEST1 = "/test"
-SlashCmdList["TEST"] = function()
-	for _, frames in pairs({"TukuiPet", "TukuiFocus", "TukuiFocusTarget", "TukuiBoss1", "TukuiBoss2", "TukuiBoss3", "TukuiBoss4"}) do
-        _G[frames].Hide = function() end
-        _G[frames]:Show()
-        _G[frames].unit = "player"
-	end
-end
-
 SLASH_TESTARENA1 = "/testarena"
 SlashCmdList["TESTARENA"] = function()
 	for _, frames in pairs({"TukuiArena1", "TukuiArena2", "TukuiArena3", "TukuiArena4"}) do
