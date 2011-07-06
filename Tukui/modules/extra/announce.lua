60d54df887c340c42b51e8d5b302a66f12bb2b7f
Jasje <bonkebesje@gmail.com>
2011-07-06 22:05:49 +0200

    Removing this script because we can handle this stuff in Tukui_SpellAlerter

--- Tukui/modules/extra/announce.lua
+++ Tukui/modules/extra/announce.lua
@@ -31,39 +31,3 @@ drinking_announce:SetScript("OnEvent", function(self, event, ...)
 		end
 	end
 end)
---------------------------------------------------
--- announce your buff/debuff(by Duffed)
---------------------------------------------------
-local f = CreateFrame("Frame")
-local function Update(self, event, ...)
-
-	local pvpType = GetZonePVPInfo()
-		if pvpType ~= "arena" then return end
-	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-		if UnitInRaid("player") and GetNumRaidMembers() > 5 then channel = "RAID" elseif GetNumPartyMembers() > 0 then channel = "PARTY" else return end
-		-- local channel = "SAY"
-		local timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
-		local spellName = select(13, ...)
-		for spell, check in pairs(SpellsAN.aura) do
-			if (eventType == "SPELL_AURA_APPLIED") and destName == UnitName("player") then
-				if spellName == spell and check == true then
-					SendChatMessage("+ "..spell.."!", channel)
-				elseif spellName == spell and check ~= true then
-					SendChatMessage(spell.."!", channel)
-				end
-			elseif eventType == "SPELL_AURA_REMOVED" and destName == UnitName("player") then
-				if spellName == spell and check ~= false then
-					SendChatMessage("- "..spell.."!", channel) 
-				end
-			end
-		end
-		for spell, check in pairs(SpellsAN.cast) do
-			if eventType == "SPELL_CAST_SUCCESS" and sourceName == UnitName("player") and spellName == spell then
-				SendChatMessage(spell.."!", channel)
-			end
-		end
-	end
-end
-f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-f:SetScript("OnEvent", Update)
