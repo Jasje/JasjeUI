64d7a49b3002ec4be04088993d7ecc2b03257280
Jasje <bonkebesje@gmail.com>
2011-07-30 09:02:28 +0200

    Removed Deathgrip from InterruptIcons spells and set size of the icons back to the old one because the apparelty overlap

--- Tukui_InterruptIcons/spells.lua
+++ Tukui_InterruptIcons/spells.lua
@@ -20,7 +20,6 @@ T.interrupt = {
 	[80964] = 10, -- feral skull bash (cat)
 	[80965] = 10, -- feral skull bash (bear)
 	[85388] = 45, -- throwdown warrior
-	[49576] = 25, -- death grip
 	[408] = 20, -- kidney shot
 	[19503] = 25, -- Scatter Shot
 }
