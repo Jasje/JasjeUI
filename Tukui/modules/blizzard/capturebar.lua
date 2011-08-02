3a029f1b61d807e0c9cd633ac80a09b925f1ef96
Jasje <bonkebesje@gmail.com>
2011-08-02 16:38:22 +0200

    Woops forgot to add the capturebar movable

--- Tukui/modules/blizzard/capturebar.lua
+++ Tukui/modules/blizzard/capturebar.lua
@@ -1,6 +1,27 @@
 local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
--- reposition capture bar to top/center of the screen
 
+local anchor = CreateFrame("Frame", "TukuiWorldStateFrameAnchor", UIParent)
+anchor:Size(170, 20)
+anchor:SetFrameStrata("TOOLTIP")
+anchor:SetFrameLevel(20)
+anchor:SetClampedToScreen(true)
+anchor:SetAlpha(0)
+anchor:SetPoint("TOP", UIParent, "TOP", 0, -35)
+anchor:SetTemplate("Default")
+anchor:SetBackdropBorderColor(1, 0, 0, 1)
+anchor:SetMovable(true)
+anchor.text = T.SetFontString(anchor, C.media.pixelfont, 8, "MONOCHROMEOUTLINE")
+anchor.text:SetPoint("CENTER")
+anchor.text:SetText(hexa.."Move WorldStateFrame")
+
+-- always show worldstate behind buffs
+WorldStateAlwaysUpFrame:SetFrameStrata("BACKGROUND")
+WorldStateAlwaysUpFrame:SetFrameLevel(0)
+WorldStateAlwaysUpFrame:ClearAllPoints()
+WorldStateAlwaysUpFrame:SetScale(1)
+WorldStateAlwaysUpFrame:SetPoint("TOP", TukuiWorldStateFrameAnchor, "BOTTOM", 0, 10)
+
+-- reposition capture bar to top/center of the screen
 local function CaptureUpdate()
 	if NUM_EXTENDED_UI_FRAMES then
 		local captureBar
@@ -9,9 +30,9 @@ local function CaptureUpdate()
 
 			if captureBar and captureBar:IsVisible() then
 				captureBar:ClearAllPoints()
-				
+
 				if( i == 1 ) then
-					captureBar:Point("TOP", UIParent, "TOP", 0, -120)
+					captureBar:Point("TOP", WorldStateAlwaysUpFrame, "BOTTOM", 0, -40)
 				else
 					captureBar:Point("TOPLEFT", getglobal("WorldStateCaptureBar" .. i - 1 ), "TOPLEFT", 0, -25)
 				end
