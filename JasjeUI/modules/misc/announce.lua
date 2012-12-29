d01842e82fd6875ced1f0e7c4925fb6deeec7f12
unknown <Gebruiker@GEBRUIK-SK7MI1L.(none)>
2012-12-29 13:11:56 +0100

    Fixed some announce things and added announce Devotion aura for Paladin

--- JasjeUI/modules/misc/announce.lua
+++ JasjeUI/modules/misc/announce.lua
@@ -1,29 +1,45 @@
-local T, C, L = unpack(Tukui)
+local T, C, L, G = unpack(Tukui)
 
+-- interrupt announce to channel /y
 local interrupt_announce = CreateFrame("Frame")
 interrupt_announce:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-interrupt_announce:SetScript("OnEvent", function(self, _, _, event, _, sourceGUID, _, _, _, _, destName, _, _, _, _, _, spellID, spellName)
-	if not (event == "SPELL_INTERRUPT" and sourceGUID == UnitGUID('player')) then return end
+interrupt_announce:SetScript("OnEvent", function(self, _, ...)
+	local _, event, _, sourceGUID, _, _, _, _, destName, _, _, _, _, _, spellID = ...
+	if not (event == "SPELL_INTERRUPT" and sourceGUID == UnitGUID("player")) then return end
 
-    SendChatMessage(INTERRUPTED.." "..destName..": \124cff71d5ff\124Hspell:"..spellID.."\124h["..spellName.."]\124h\124r!", "YELL", nil, nil) 
-    end)
+	SendChatMessage(INTERRUPTED.." "..destName..": "..GetSpellLink(spellID), "YELL")
+end)
 
+-- drink announce for arena	
 local drinking_announce = CreateFrame("Frame")
 drinking_announce:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 drinking_announce:SetScript("OnEvent", function(self, event, ...)
 	if not (event == "UNIT_SPELLCAST_SUCCEEDED" and GetZonePVPInfo() == "arena") then return end
 
-	local unit, spellName, spellrank, spelline, spellID = ...
-	if UnitIsEnemy("player", unit) and (spellID == 81923 or spellName == "Drinking") then
-		if GetRealNumRaidMembers() > 0 then
-			SendChatMessage(UnitName(unit).." is drinking.", "RAID")
-			PlaySoundFile("Interface\\Addons\\Tukui_SpellAlert\\sounds\\81923.mp3", "Master")
-		elseif GetRealNumPartyMembers() > 0 and not UnitInRaid("player") then
-			SendChatMessage(UnitName(unit).." is drinking.", "PARTY")
-			PlaySoundFile("Interface\\Addons\\Tukui_SpellAlert\\sounds\\81923.mp3", "Master")
-		else
-			SendChatMessage(UnitName(unit).." is drinking.", "SAY")
-			PlaySoundFile("Interface\\Addons\\Tukui_SpellAlert\\sounds\\81923.mp3", "Master")
+	local unit, _, _, _, spellID = ...
+	if UnitIsEnemy("player", unit) and (spellID == 80167 or spellID == 94468 or spellID == 43183 or spellID == 57073) then
+		SendChatMessage(UnitName(unit).."  is drinking.", "PARTY")
+		PlaySoundFile("Interface\\Addons\\Tukui_SpellAlert\\sounds\\81923.mp3", "Master")
+	end
+end)
+
+-- announce Devotion Aura - credit wrufu
+local f = CreateFrame("Frame")
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+f:SetScript("OnEvent", function(self, event, ...)
+local event, destGUID, id = select(2, ...), select(8, ...)
+	if (event == "SPELL_AURA_APPLIED" and destGUID == UnitGUID("player")) then
+		id = select(12, ...)
+		if id == 31821 then 
+			local msg = format("%s %s", GetSpellLink(id), "is up.")
+			SendChatMessage(msg, "SAY")
+		end
+	end
+	if (event == "SPELL_AURA_REMOVED" and destGUID == UnitGUID("player")) then
+		id = select(12, ...)
+		if id == 31821 then 
+			local msg = format("%s %s", GetSpellLink(id), "has faded.")
+			SendChatMessage(msg, "SAY")
 		end
 	end
 end)
